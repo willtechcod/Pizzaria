2efd00568887e3dbcebb71edc2e70dd53b404abd
willtechcod <wills@willtech>
2021-07-20 22:05:23 -0300

    firts commit

---
+++ js/master.js
@@ -0,0 +1,12 @@
+
+//var fm = document.forms.length;
+//document.write(fm);
+
+function salvarpedido(){
+    var nome  = document.getElementById("nome").value;
+    var tefefone = document.getElementById("telefone").value;
+    var endereco = document.getElementById("endereco").value;
+    
+    alert("você fez o Pedido:"+ nome + tefefone + endereco);
+    
+}
